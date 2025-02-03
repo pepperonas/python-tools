5bef1ca6c95a44658d77d9cd3e261770668f4d1b
martin <martinpaush@gmail.co>
2025-02-03 15:53:59 +0100

    - added screen clicker with sound and cursor reset

---
+++ the-click-so-chic-secondary-reset-sound.py
@@ -0,0 +1,75 @@
+# Voraussetzungen
+# pip install pyautogui screeninfo
+
+import time
+import datetime
+import random
+import pyautogui
+from screeninfo import get_monitors
+import winsound
+
+notes = {
+    'C': 261,  # C-Dur
+    'D': 294,  # D-Dur
+    'E': 330,  # E-Dur
+    'F': 349,  # F-Dur
+    'G': 392,  # G-Dur
+    'A': 440,  # A-Dur
+    'B': 493,  # H-Dur
+    'C2': 523  # C2-Dur (eine Oktave höher)
+}
+
+melody = [
+    ('E', 0.5),('E', 0.5),('F', 0.5),('G', 0.5),('G', 0.5),('F', 0.5),('E', 0.5),('D', 0.5),('C', 1.0),
+    ('C', 0.5),('D', 0.5),('E', 0.5),('E', 0.5),('E', 1.0),('D', 0.5),('D', 0.5),
+    ('E', 0.5),('F', 0.5),('G', 0.5),('G', 0.5),('F', 0.5),('E', 0.5),('D', 0.5),('C', 1.0)
+]
+
+def play_sound():
+    for note, duration in melody:
+        # Spiele den Ton (Frequenz und Dauer)
+        winsound.Beep(notes[note], int(duration * 500))  # Dauer in 1/2 Millisekunden
+        time.sleep(0.1)  # Kleine Pause zwischen den Tönen
+        
+def play_beep():
+    frequency = 3000  # 1000 Hz
+    duration = 60    # 200 ms
+    winsound.Beep(frequency, duration)
+            
+def get_monitor():
+    monitors = get_monitors()
+    print(f"Montior(e): {len(monitors)}")
+    return monitors[0]; # AUSKOMMENTIEREN WENN ZWEITER MONITOR GEKLICKT WERDEN SOLL
+    if len(monitors) > 1:
+        return monitors[1]  # Der zweite Monitor (Index 1)
+    else:
+        print("Kein sekundärer Monitor gefunden.")
+        return None
+
+def click_middle_of_monitor():
+    monitor = get_monitor()
+    if monitor:
+        middle_x = monitor.x + monitor.width // 2
+        middle_y = (monitor.y + monitor.height // 2) + (monitor.height * 10 / 100)
+
+        while True:
+            play_sound()            
+            
+            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+
+            x, y = pyautogui.position()
+            pyautogui.click(middle_x, middle_y)  # Klick in der Mitte des sekundären Monitors
+            
+            play_beep()            
+            
+            print(f"{current_time} - Klick({middle_x}|{middle_y}) - Monitor 2")
+            time.sleep(1)  # x Sekunden warten
+            pyautogui.moveTo(x, y) # Mouse zurücksetzen
+            pyautogui.click(x, y)
+            
+            sleep_time = random.randint(180, 355)
+            print(f"Nächster Klick in {sleep_time} Sekunden")
+            time.sleep(sleep_time)  # x Sekunden warten
+
+if __name__ == "__main__":
+    click_middle_of_monitor()
